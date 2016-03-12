3ce6af4963eaca9af63a77d085ee61b961fec30f
atwalharvey93@gmail.com <harveya@sfu.ca>
2016-03-12 14:47:31 -0800

    Configured bower

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public')).listen(8080);
+var config = require('./config');
+
+var app = connect();
+
+app.use(serveStatic(__dirname + '/public'));
+app.use('/bower_components', serveStatic(__dirname + '/bower_components'));
+
+app.listen(config.port);
